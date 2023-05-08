495ad39790b4187caca27bb84bb04f464274176a
ssuuga <132870651+ssuuga@users.noreply.github.com>
2023-05-08 18:50:08 -0300

    Add files via upload

--- Copias/sketch.js
+++ Copias/sketch.js
@@ -1,31 +1,37 @@
 let figuras = [];
 let distanciaEntreFiguras = 50; // distancia fija entre las figuras
 let escalaMinima = 0.5; // escala mínima de las figuras
-let escalaMaxima = 2; // escala máxima de las figuras
+let escalaMaxima = 1.5; // escala máxima de las figuras
 
 let formaActual = "circulo";
 
 let angle = 0; // VARIABLE QUE SERVIRÁ PARA DEFINIR EL ÁNGULO DE ROTACIÓN DE LAS FIGURAS
 
 
+let colorDeFondo; //variable global de color celeste
+let colorDeFondo2; //variable global de color rojo
+let colorActual; // Variable que almacena el color actual
+let cambioColor = false; // Variable de bandera para el cambio de color
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
    // LAS FIGURAS TIENEN COMO PUNTO DE ORIGEN SU CENTRO
 
-
-   
-
-
   // Creamos la primera y última figura con tamaños definidos
   let primeraFigura = new Figura(0, 0, 30);
   let ultimaFigura = new Figura(0, 0, 60);
+  
+  colorDeFondo = color(80, 150, 200); // Definir color celeste
+  colorDeFondo2 = color(200, 80, 60); // Definir color rojo
+  colorActual = colorDeFondo; // Establecer el color inicial
 
   // Calculamos la escala máxima y mínima de las figuras intermedias
   let escalaMinimaIntermedia = escalaMinima * primeraFigura.size / ultimaFigura.size;
   let escalaMaximaIntermedia = escalaMaxima * primeraFigura.size / ultimaFigura.size;
 
   // Creamos las figuras intermedias con una escala que varía entre la mínima y máxima calculadas
-  for(let i = 1; i < 20; i++) {
+  for(let i = 1; i < 19; i++) {
     let escalaIntermedia = map(i, 1, 20, escalaMinimaIntermedia, escalaMaximaIntermedia);
     let nuevaFigura = new Figura(0, 0, primeraFigura.size * escalaIntermedia * i);
     figuras.push(nuevaFigura);
@@ -35,8 +41,6 @@ function setup() {
   // ANGULO EN GRADOS
   angleMode( DEGREES );
   frameRate( 60 ); // DEFINO UN NUMERO ESTABLECIDO DE FOTOGRAMAS
-
- 
 }
 
 function draw() {
@@ -48,21 +52,20 @@ function draw() {
   }
 }
 
-
 function keyPressed() {
-  if (key == "1") { // Tecla 1
+  if (key == "1") {        // Tecla 1
     formaActual = "cuadrado";
   } else if (key == "2") { // Tecla 2
     formaActual = "circulo";
   } else if (key == "3") { // Tecla 3
     formaActual = "triangulo";
-  } else if (key == "4") { // Tecla 3
+  } else if (key == "4") { // Tecla 4
     formaActual = "pentagono";
-  } else if (key == "5") { // Tecla 3
+  } else if (key == "5") { // Tecla 5
     formaActual = "hexagono";
-  } else if (key == "6") { // Tecla 3
+  } else if (key == "6") { // Tecla 6
     formaActual = "galleta";
-  } else if (key == "7") { // Tecla 3
+  } else if (key == "7") { // Tecla 7
     formaActual = "estrella";
   }
 }
@@ -86,86 +89,105 @@ class Figura {
 
   display(indice, cantidad) {
     // calculamos el valor de transparencia de la figura
-    let transparencia = map(indice, 0, cantidad-1, 255, 0);
-    let colorDeFondo = color(80, 150, 200, transparencia);
-    let colorDeBorde = lerpColor(colorDeFondo, color(255), 0.5);
+    let transparencia = map(indice, 0, cantidad - 1, 255, 0);
+    colorActual.setAlpha(transparencia);
+  fill(colorActual);
 
-    // dibujamos la figura con el gradiente de color cele
   // dibujamos la figura con el gradiente de color celeste
   noStroke();
-  fill(colorDeFondo);
+ // fill(currentColor);
   ellipseMode(CENTER);
   
   
-  
+  //---------------------FUNCIONAMIENTO DE LAS 7 FIGURAS---------------------//
   switch (formaActual) {
     case "cuadrado":
-      rotate(angle + 5 * (width/2 +- mouseX) / width)
+      rotate(angle + 8 * (width/2 +- mouseX) / width)
       rectMode(CENTER);
       rect(this.x, this.y, this.size, this.size);
       break;
+
     case "circulo":
-      rotate(angle + 5 * (width/2 +- mouseX) / width)
+      rotate(angle + 15 * (width/3 +- mouseX) / width)
       ellipseMode(CENTER);
-      ellipse(this.x, this.y, this.size, this.size + 150);
+      ellipse(this.x * sin(angle), this.y, this.size + 45, this.size + 15);
       break;
+
     case "triangulo":
       rotate(angle + 5 * (width/2 +- mouseX) / width)
-      
-      triangle(this.x, this.y - this.size / 2, this.x - this.size / 2, this.y + (this.size +15) / 2, this.x + this.size / 2, this.y + (this.size +15) / 2);
+      beginShape();
+      for (let i = 0; i < 5; i++) {
+        let angulo = 600  * i - 5;
+        let x = this.x + ((cos(angulo) * (this.size * 0.7))); //aumento de tamaño en x
+        let y = this.y + ((sin(angulo) * (this.size * 0.7))); //aumento de tamaño en y
+        vertex(x, y);
+      }
+      endShape(CLOSE);
       break;
 
     case "pentagono":
         rotate(angle + 5 * (width/2 +- mouseX) / width)
-
         beginShape();
 
         for (let i = 0; i < 5; i++) {
           let angulo =  i * 360 /5;
-          let x = ((this.x ) + cos(angulo) * (this.size/2));
-          let y = ((this.y ) + sin(angulo) * (this.size/2));
+          let x = ((this.x ) + cos(angulo) * (this.size * 0.7)); //aumento de tamaño en x
+          let y = ((this.y ) + sin(angulo) * (this.size * 0.7)); //aumento de tamaño en y
           vertex(x, y);
         }
         endShape(CLOSE);
         break;
+
     case "hexagono":
         rotate(angle + 5 * (width/2 +- mouseX) / width)
-
         beginShape();
        
         for (let i = 0; i < 6; i++) {
           let angulo =  i * 360 / 6;
-          let x = this.x + ((cos(angulo) * (this.size/2)));
-        let y = this.y + ((sin(angulo) * (this.size/2)));
+          let x = this.x + ((cos(angulo) * (this.size * 0.7))); //aumento de tamaño en x
+          let y = this.y + ((sin(angulo) * (this.size * 0.7))); //aumento de tamaño en y
           vertex(x, y);
         }
         endShape(CLOSE);
-        break;  
+        break;
+
     case "galleta":
       rotate(angle + 5 * (width/2 +- mouseX) / width)
 
       beginShape();
-      let radio = this.size;
+      //let radio = this.size;
       for (let i = 0; i < 360; i++) {
         let angulo = 100  - i * 50;
-        let x = this.x + ((cos(angulo) * (this.size/2)));
-        let y = this.y + ((sin(angulo) * (this.size/2)));
+        let x = this.x + ((cos(angulo) * (this.size * 0.7))); //aumento de tamaño en x
+        let y = this.y + ((sin(angulo) * (this.size * 0.7))); //aumento de tamaño en y
         vertex(x, y);
       }
       endShape(CLOSE);
     break;
       
     case "estrella":
+      rotate(angle + 5 * (width/2 +- mouseX) / width)
       beginShape();
-      for (let i = 0; i < 5; i++) {
-        let angulo = 500  * i - 5;
-        let x = this.x + ((cos(angulo) * (this.size/2)));
-        let y = this.y + ((sin(angulo) * (this.size/2)));
+      for (let i = 0; i < 100; i++) {
+        let angulo = (mouseX * 2.5) * i/30;
+        let x = this.x + ((cos(angulo) * (this.size * 0.7))); //aumento de tamaño en x
+        let y = this.y + ((sin(angulo) * (this.size * 0.7))); //aumento de tamaño en y
         vertex(x, y);
       }
       endShape(CLOSE);
     break;
       }
+  }
+}
+
+function mouseClicked() {
+  // Cambia entre los dos colores al hacer clic
+  cambioColor = !cambioColor; // Invierte el valor de la bandera
+
+  if (cambioColor) {
+    colorActual = colorDeFondo2;
+  } else {
+    colorActual = colorDeFondo;
   }
 }
 
